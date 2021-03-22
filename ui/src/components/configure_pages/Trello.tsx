7f8974756ad04540d9bb242d96e26048f83899cf
Wes Turner <50891+westurner@users.noreply.github.com>
2021-03-21 22:54:29 -0400

    Revert "DOC: configure_pages/Trello.tsx: typo (#343)"
    This reverts commit e4a9753754a8a8d824f8e4f2e00f49f6c51b5e9f.

--- ui/src/components/configure_pages/Trello.tsx
+++ ui/src/components/configure_pages/Trello.tsx
@@ -103,7 +103,7 @@ const Trello: FunctionComponent = () => {
                   <span className="code">intheamtrellolistname</span>.
                 </li>
                 <li>
-                  You can change the description of a task by changing the value
+                  You can change the description of a task by changing he value
                   of the field{' '}
                   <span className="code">intheamtrellodescription</span>.
                 </li>
